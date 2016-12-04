54188f1b807970e1ebc009ab678c8cb9fec32aa3
Steven Kardell <steven-kardell@uiowa.edu>
2016-12-04 23:54:05 +0000

    getting error when loading schema to heroku

--- db/migrate/20161120042733_create_households.rb
+++ db/migrate/20161120042733_create_households.rb
@@ -2,7 +2,6 @@ class CreateHouseholds < ActiveRecord::Migration[5.0]
   def change
     create_table :households do |t|
       t.string :name
-      t.integer :code
       t.timestamps
     end
   end
