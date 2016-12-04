f66704139001df57d8bd1604c46bfdb8365d472f
Steven Kardell <steven-kardell@uiowa.edu>
2016-12-04 23:37:08 +0000

    2nd try

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: @gmail.com,
+    domain: "@gmail.com",
     user_name: "xxxxxxxxxxxx@gmail.com",
     password: "xxxxxxxxxxxxx",
     authentication: "plain",
