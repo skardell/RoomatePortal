74c70c5faa655b93dc977974f362c879c615854a
Steven Kardell <steven-kardell@uiowa.edu>
2016-12-05 19:06:10 +0000

    added my email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,19 +52,21 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'shielded-waters-90884.herokuapp.com' }
   
   config.action_mailer.delivery_method = :smtp
-  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "@gmail.com",
-    user_name: "xxxxxxxxxxxx@gmail.com",
-    password: "xxxxxxxxxxxxx",
-    authentication: "plain",
-    enable_starttls_auto: true
-    
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
   }
   
 end
