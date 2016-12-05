7ddb25c24e80bab7312f79e200477490a27f2441
Steven Kardell <steven-kardell@uiowa.edu>
2016-12-05 22:14:51 +0000

    adding household key field

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -44,7 +44,7 @@ before_action :configure_sign_up_params, only: [:create]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:household_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:household_name, :household_key])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
