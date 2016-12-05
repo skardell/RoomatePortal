82fcba97b8aeca8486c46fc1654d6ae0f803ac11
Steven Kardell <steven-kardell@uiowa.edu>
2016-12-05 19:01:34 +0000

    added my email

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,6 +1,6 @@
 class InviteMailer < ApplicationMailer
     
     def invite_send(email)
-        mail to: email, subject: "Household Invite", from: 'info@mysite.com'
+        mail to: email, subject: "Household Invite", from: 'srkardell@gmail.com'
     end
 end
