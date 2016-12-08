3453c1eb71d0b0ecf72396150e144e44d381d9a8
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-12-08 23:57:02 +0000

    login and signup route tests functional

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,5 +3,5 @@ When(/^I click on the Login link$/) do
 end
 
 Then(/^I should see the login form$/) do
-  pending
+  find(:xpath, '/html/body/h2').should have_content('Log in')
 end
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,3 +1,8 @@
 When(/^I click on the Signup link$/) do
   click_on 'Signup'
 end
+
+Then(/^I should see the registration form$/) do
+  find(:xpath, '/html/body/h2').should have_content('Sign up')
+end
+
