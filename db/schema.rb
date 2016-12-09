3699fe928a8ff8438178f6b4a2a65ec2a50e952f
Chris Choi <christopher-choi@uiowa.edu>
2016-12-09 03:03:47 -0600

    signup/login view edit, added requestor to chores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208184504) do
+ActiveRecord::Schema.define(version: 20161209080647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161208184504) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "description"
+    t.string   "requestor"
     t.index ["household_id"], name: "index_chores_on_household_id", using: :btree
     t.index ["user_id"], name: "index_chores_on_user_id", using: :btree
   end
