cd8b11f5c5f7716693251cc329fb6b4bbf61edea
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-12-09 13:25:50 +0000

    pending steps

--- features/step_definitions/grocery_steps.rb
+++ features/step_definitions/grocery_steps.rb
@@ -1,51 +1,51 @@
 Given(/^I am signed into my account$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When(/^I am on the Groceries page$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should see the groceries lists$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When(/^I click on the "Add Grocery Item" link$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should be on the add grocery item page$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given(/^I am on the add grocery item page$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When(/^I fill in the grocery information forms$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When(/^I click the "Add" button$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When(/^I click on the "Cancel" button$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should return to the Groceries page$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should see my added grocery items$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When(/^I click on the X associated with a grocery item$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should not see the grocery item in the grocery list$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
