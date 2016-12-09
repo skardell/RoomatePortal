b15402c0e755653f632ed10f93cb6a2aa0d90632
harrison <harrison-genders@uiowa.edu>
2016-12-09 08:43:25 +0000

    added testing

---
+++ features/step_definitions/chore_steps.rb
@@ -0,0 +1,96 @@
+When(/^I visit the Chores page$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the list of Household Chores$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am on the Chores page$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on the "Create Chore" link$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be on the chore creation page$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am on the chore creation page$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill in the information forms$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click the "Create" button$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should return to the Chores page$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see my created chore in the chores list$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on an incomplete chore$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be on that chore's information page$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can return to the Chores page by clicking the "Back" button$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can accept the chore by clicking the "Accept" button$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on an accepted chore$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can see who accepted the chore$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on a chore I accepted$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can see that I accepted the chore$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can complete the chore by clicking the "Mark As Complete" button$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click the "Mark As Complete" button$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should return to the Chore page$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^My chore should be marked as completed$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+When(/^I click on a completed chore$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I can see who completed the chore$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
