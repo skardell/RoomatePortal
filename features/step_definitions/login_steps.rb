5ed0e5d45ff0c7cec506a7ae903faac0af2333b0
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-12-09 12:49:48 +0000

    made sign in step dynamic

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -11,25 +11,13 @@ Given(/^A user with the email "email@email.com" and password "password"$/) do
   user.save
 end
 
-When(/^I sign in with email "email@email.com" and password "password"$/) do
-  fill_in 'user_email', :with => 'email@email.com'
-  fill_in 'user_password', :with => 'password'
-  click_on 'Log in'
-end
-
 Then(/^I should be on my household dashboard$/) do
   visit "dashboard"
 end  
 
-When(/^I sign in with email "notmyemail@email.com" and password "password"$/) do
-  fill_in 'user_email', :with => 'notmyemail@email.com'
-  fill_in 'user_password', :with => 'password'
-  click_on 'Log in'
-end
-
-When(/^I sign in with email "email@email.com" and password "notmypassword"$/) do
-  fill_in 'user_email', :with => 'email@email.com'
-  fill_in 'user_password', :with => 'notmypassword'
+When /^I sign in with email "(.*?)" and password "(.*?)"$/ do |email, password|
+  fill_in 'user_email', :with => email
+  fill_in 'user_password', :with => password
   click_on 'Log in'
 end
 
