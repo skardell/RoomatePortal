2e8c0f6481d1bcdadd7284745895560142dc8292
harrison <harrison-genders@uiowa.edu>
2016-12-09 02:42:13 +0000

    added testing for login

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -17,20 +17,10 @@ When(/^I sign in with email "email@email.com" and password "password"$/) do
   click_on 'Log in'
 end
 
-Given(/^A user with the email "email@email.com" and password "password"$/) do
-  user = FactoryGirl.create(:user)
-  user.save
-end
-
 When(/^I sign in with email "notmyemail@email.com" and password "password"$/) do
   fill_in 'user_email', :with => 'notmyemail@email.com'
   fill_in 'user_password', :with => 'password'
   click_on 'Log in'
-end
-
-Given(/^A user with the email "email@email.com" and password "password"$/) do
-  user = FactoryGirl.create(:user)
-  user.save
 end
 
 When(/^I sign in with email "email@email.com" and password "notmypassword"$/) do
